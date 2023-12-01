e398bcc4cf70269d1bfac0ea5eaa73ac319fb455
Elena <0koshko0@gmail.com>
2023-12-01 13:28:34 +0300

    chenge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,52 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        FullStack devF = new FullStack();
-        devF.developGUI();
-        devF.developServer();
+        Random rand = new Random();
+        List<Developer> developers = new ArrayList<>();
 
-        Developer dev = new Frontender();
-        ((FrontAction) dev).developGUI();
+        int tempInt = rand.nextInt(0, 100);
+        for (int i = 0; i < tempInt; i++) {
+            if (rand.nextBoolean()) {
+                developers.add(new Frontender());
+            } else {
+                developers.add(new Frontender());
+            }
+
+        }
+
+        for (Developer developer : developers) {
+            checkSpec(developer);
+        }
+
+        var test1 = new BackendAction() {
+            @Override
+            public void developServer() {
+
+            }
+        };
+
+
+        var test2 = new BackendAction() {
+            @Override
+            public void developServer() {
+
+            }
+        };
+
+        System.out.println(test1.getClass().toString());
+        System.out.println(test2.getClass().toString());
+    }
+
+    static void checkSpec(Developer developer) {
+        if (developer instanceof FrontAction) {
+            ((Frontender) developer).developGUI();
+        } else {
+            System.out.println("Create GUI not supported by this Developer");
+        }
+
     }
 }
